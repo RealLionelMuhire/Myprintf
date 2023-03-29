bf4717cab3e48906d757d2a7640ac2cd420690f8
RealLionelMuhire <muhirelionel@gmail.com>
2023-03-29 11:34:09 +0200

    All Tasks. Files: main.h test.c _putchar.c

---
+++ gptmain.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+int main(void)
+{
+    int count;
+
+    count = _printf("Hello, world!\n");
+    _printf("Number of characters printed: %d\n", count);
+
+    return (0);
+}
+
---
+++ test.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <limits.h>
+#include <stdio.h>
+
+void main(void)
+{
+	_printf("Hello World\n");
+	printf("Hello World\n");
+}
